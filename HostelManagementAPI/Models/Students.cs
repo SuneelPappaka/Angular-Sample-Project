4d894932cad4be51dc385aa776910ce047ec2b04
SuneelPappaka <pappakasuneel123@gmail.com>
2026-08-01 16:05:06 +0530

    Model

--- HostelManagementAPI/Models/Students.cs
+++ HostelManagementAPI/Models/Students.cs
@@ -2,18 +2,19 @@
 {
     public class Students
     {
-        public int  id{get; set; }
+        public int id { get; set; }
 
-        public string  name{ get; set; }
+        public string name { get; set; }
 
         public string email { get; set; }
 
-        public string  phone{ get; set; }
- 
-        public int roomNo{ get; set; }
+        public string phone { get; set; }
+
+        public int roomNo { get; set; }
 
         public string course;
 
         public string gender { get; set; }
-    
+
+    }
 }
